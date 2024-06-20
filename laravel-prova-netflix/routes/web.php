e0dc78bdab62edd278f19d173426f9de52854fbb
Nicolae <nicolaibarbarasa@gmail.com>
2024-06-20 18:39:48 +0200

    Install Laravel Breeze with Blade and Alpine, add authentication pages

--- laravel-prova-netflix/routes/web.php
+++ laravel-prova-netflix/routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MovieController;
 
-// Rotta per visualizzare i film popolari
-Route::get('/', [MovieController::class, 'index'])->name('movies.index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Rotta per cercare i film
-Route::get('/movies/search', [MovieController::class, 'search'])->name('movies.search');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
